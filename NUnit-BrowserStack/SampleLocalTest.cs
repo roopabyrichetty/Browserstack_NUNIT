d92088f38493fc7d4f80b6a4e09ee79a62486512
Roopa Byrichetty <roopab77@yahoo.com>
2023-07-20 13:37:17 -0400

    Initial Commit

---
+++ NUnit-BrowserStack/SampleLocalTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Text.RegularExpressions;
+
+namespace BrowserStack
+{
+  [TestFixture]
+  [Category("sample-local-test")]
+  public class SampleLocalTest : BrowserStackNUnitTest
+  {
+    public SampleLocalTest() : base() { }
+
+    [Test]
+    public void BStackTunnelCheck()
+    {
+      driver.Navigate().GoToUrl("http://www.bentley.com/");
+      StringAssert.Contains("Home | Bentley | Infrastructure Engineering Software", driver.Title);
+    }
+  }
+}
---
+++ NUnit-BrowserStack/SampleTest.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace BrowserStack
+{
+    [TestFixture]
+    [Category("sample-test")]
+    public class SampleTest : BrowserStackNUnitTest
+    {
+        public SampleTest() : base() { }
+
+        [Test]
+        public void SearchBstackDemo()
+        {
+            driver.Navigate().GoToUrl("https://www.bentley.com/");
+            Assert.AreEqual("Home | Bentley | Infrastructure Engineering Software", driver.Title);          
+
+            
+        }
+    }
+}
